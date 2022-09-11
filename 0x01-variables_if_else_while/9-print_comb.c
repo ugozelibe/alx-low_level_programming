041e3ecc2d0f0ab8b21da4e9413191e08261b593
ugozelibe <ugozee4eternity@gmail.com>
2022-09-11 14:31:31 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,14 @@ int main(void)
 {
 int c;
 for (c = 48; c <= 57; c++)
-{ 
-putchar(c);
-}
+{
+	putchar(c);
 if (c != 57)
-{  
-	putchar(',');
-	putchar(' ');
+{
+	putchar(',');									putchar(' ');
+}
 }
 putchar('\n');
 return (0);
 }
+
