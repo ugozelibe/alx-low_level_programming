01ea16217288648b5080aa68d0fa1bad8ff8994b
ugozelibe <ugozee4eternity@gmail.com>
2022-09-20 04:08:00 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main  Printing in C language
+ * main Printing in C language
  *
  * Return: Always 0 (Accurate)
  */
 int main(void)
-
-  {
-	 puts("\"Programming is like building a multilingual puzzle");
-	 return (0);
-  }
+{
+	puts("Programming is like building a multilingual puzzle \n");
+return (0);
+}
